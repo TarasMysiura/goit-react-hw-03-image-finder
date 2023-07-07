4d9d8c77cd0a7a01f91e9dbe352e144cc10deabb
TarasMysiura <tarasmysiura@gmail.com>
2023-07-07 14:25:49 +0300

    ++

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,54 @@
+import axios from 'axios';
+
 import { AppStyle } from 'components/App.styled';
 import { Searchbar } from './Searchbar/Searchbar';
 import './styles.css';
+import { ImageGallery } from './ImageGallery/ImageGallery';
+import { Component } from 'react';
+import { fetchImages } from 'services/Api';
 
-export const App = () => {
+export class App extends Component{
+  state = {
+    modal: { isOpen: false, visibleData: null },
+    images: [],
+    isLoading: false,
+    error: null,
+    selectedPostId: null,
+  };
+
+  // onOpenModal = data => {
+  //   this.setState({
+  //     modal: {
+  //       isOpen: true,
+  //       visibleData: data,
+  //     },
+  //   });
+  // };
+
+  // onCloseModal = () => {
+  //   this.setState({
+  //     modal: {
+  //       isOpen: false,
+  //       visibleData: null,
+  //     },
+  //   });
+  // };
+  
+  async componentDidMount() {
+    try {
+      this.setState({ isLoading: true });
+      const posts = await fetchImages();
+      this.setState({ posts });
+      // toast.success('Your posts were successfully fetched!', toastConfig);
+    } catch (error) {
+      this.setState({ error: error.message });
+      // toast.error(error.message, toastConfig);
+    } finally {
+      this.setState({ isLoading: false });
+    }
+  }
+  
+  
   return (
     <AppStyle
     // style={{
@@ -15,6 +61,7 @@ export const App = () => {
     // }}
     >
       <Searchbar />
+      <ImageGallery/>
     </AppStyle>
   );
 };
